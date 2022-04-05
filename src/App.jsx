cb33f17bfcc43b159844fa181a855017f769de85
Anand <anand21mn@gmail.com>
2022-04-05 19:42:21 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import './charts/ChartjsConfig';
 
 // Import pages
 import Dashboard from './pages/Dashboard';
+import Test from './pages/Test';
 
 function App() {
 
@@ -26,6 +27,7 @@ function App() {
     <>
       <Routes>
         <Route exact path="/" element={<Dashboard />} />
+        <Route path="/unity/:id/test"  element={<Test />} />
       </Routes>
     </>
   );
